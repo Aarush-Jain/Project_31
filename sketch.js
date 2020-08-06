363233cdf89f2a68fc04aad9c055874c419a4371
Aarush-Jain <ajjacky1909@gmail.com>
2020-08-06 20:00:35 +0530

    committed

--- sketch.js
+++ sketch.js
@@ -1,9 +1,71 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var ground;
+var particles = [];
+var plinkos = [];
+var divisions = [];
+var divisionHeight = 300;
+var groundSprite;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(480,800);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+  ground = new Ground(240,790,480,20);
+
+  for(var k=0; k<=width; k=k+80){
+    divisions.push(new Divisions(k,height - divisionHeight/2,10,divisionHeight));
+  }
+
+  for(var j=40; j<=width; j=j+50){
+    plinkos.push(new Plinko(j,75,10))
+  }
+  
+  for(var j=15; j<=width-10; j=j+50){
+    plinkos.push(new Plinko(j,175,10))
+  }
+
+  for(var i=40; i<=width; i=i+50){
+    plinkos.push(new Plinko(i,275,10))
+  }
+  
+  for(var i=15; i<=width-10; i=i+50){
+    plinkos.push(new Plinko(i,375,10))
+  }
+
+  if(frameCount % 60 === 0){
+    particles.push(new Particle(200,10,10))
+  }
+
+  groundSprite = createSprite(240,790,480,20);
+
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black"); 
+
+  Engine.update(engine); 
+
+  groundSprite.shapeColor = "yellow";
+
+  for(var i = 0; i < plinkos.length;i++){
+    plinkos[i].display()
+  }
+
+  for(var j = 0; j < divisions.length;j++){
+    divisions[j].display()
+  }
+
+  for(var a = 0; a < particles.length;a++){
+    particles[a].display()
+  }
+
   drawSprites();
+  
 }
